51c0622719a50a0fb3b6035e61e2b8135cec69c8
Cody McCodePants <cody@mccodepants.com>
2022-12-11 16:45:18 +0800

    added middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/lsyurea/Web-application-with-go/pkg/config"
 	"github.com/lsyurea/Web-application-with-go/pkg/handlers"
 )
@@ -10,6 +11,9 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
