8150964a5601b7f2c7da83e6bb6ed6f1d28d5318
Cody McCodePants <cody@mccodepants.com>
2022-12-11 17:35:17 +0800

    clean up middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 	"github.com/justinas/nosurf"
 )
-
-func WriteToConsole(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hit the page")
-		next.ServeHTTP(w, r)
-	})
-}
 
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
